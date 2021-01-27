e82933e12589d63c2cb32e1820b52836cf5b2b19
周辰罡 <50517419+zhouchengang@users.noreply.github.com>
2021-01-28 02:32:25 +0800

    Update t.js

--- t.js
+++ t.js
@@ -8,5 +8,17 @@ var store = oss({
 });
 
 //client.useBucket('backupzcg');
-let signUrl = store.signatureUrl('xxx.jpg', {expires: 60, 'process' : 'style/jpg'});
-console.log("signUrl="+signUrl);
+//let signUrl = store.signatureUrl('xxx.jpg', {expires: 60, 'process' : 'style/jpg'});
+//console.log("signUrl="+signUrl);
+
+async function list () {
+  try {
+    let result = await client.list({
+      'max-keys': 5
+    })
+    console.log(result)
+  } catch (err) {
+    console.log (err)
+  }
+}
+list();
