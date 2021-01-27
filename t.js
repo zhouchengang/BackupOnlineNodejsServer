9a4f3156ba59f12a86843aaf166ab4b703aa3cdd
周辰罡 <50517419+zhouchengang@users.noreply.github.com>
2021-01-28 01:47:21 +0800

    Update t.js

--- t.js
+++ t.js
@@ -1,1 +1,21 @@
- 
+var oss = require('ali-oss');
+
+var store = oss({
+  accessKeyId: 'your access key',
+  accessKeySecret: 'your access secret',
+  bucket: 'your bucket name',
+  region: 'oss-cn-hangzhou'
+});
+
+client.useBucket('backupzcg');
+async function list () {
+  try {
+    let result = await client.list({
+      'max-keys': 5
+    })
+    console.log(result)
+  } catch (err) {
+    console.log (err)
+  }
+}
+list();
