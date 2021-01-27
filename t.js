7a03c3b9f28c83f37bdb31a73152127d2a3364f5
周辰罡 <50517419+zhouchengang@users.noreply.github.com>
2021-01-28 02:28:43 +0800

    Update t.js

--- t.js
+++ t.js
@@ -1,10 +1,10 @@
 var oss = require('ali-oss');
 
-const store = new oss({
+var store = oss({
   accessKeyId: 'LTAI4G1QUQQJC2dHuvVfoq21',
   accessKeySecret: 'z5fPIHRvzFoTHrJyfHDRfXE10jNxqI',
   bucket: 'backupzcg',
-  endpoint: 'https://oss-cn-hongkong.aliyuncs.com'
+  region: 'oss-cn-hongkong'
 });
 
 //client.useBucket('backupzcg');
